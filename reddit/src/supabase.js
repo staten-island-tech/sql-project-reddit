12a286886445117d6d1029d1c2813c0535b06fa0
saharvitale <113132357+saharvitale@users.noreply.github.com>
2023-05-22 14:00:09 -0400

    register.vue script

--- reddit/src/supabase.js
+++ reddit/src/supabase.js
@@ -5,7 +5,8 @@ import { createClient } from '@supabase/supabase-js'
 // not required however as they are 100% exposed on the client side anyway
 // and that's ok, Supabase expects this (security is provided by Row Level Security)
 const supabaseUrl = 'https://epxetgstlknrdacnxswy.supabase.co'
-const supabaseKey = ''
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVweGV0Z3N0bGtucmRhY254c3d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3NDEyMzQsImV4cCI6MTk5OTMxNzIzNH0.vJSlWpAy8_O1QG6qRXRlcX_I1DgHQW5Pltav4zti12Y'
 
 // setup client
 const supabase = createClient(supabaseUrl, supabaseKey)
